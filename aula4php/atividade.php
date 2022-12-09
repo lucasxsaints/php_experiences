d77f4cf4c5187d7651edc888d16814af96570c24
lucas caetano <lucasxsaints@hotmail.com>
2022-12-09 01:00:38 -0300

    kinda done

---
+++ aula4php/atividade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      <h2> <.Faça um Programa que peça um número e então mostre 
+         a mensagem “O número informado foi [número].”> </h2>
+       
+     
+        <form action = "numero" method= "get">
+    
+     Digite o um numero <br/>
+        <input type="text" name= "numero" id= "" />
+        <br/> <br/>
+    
+      </form>
+    
+    
+    
+    <?php
+    
+    $n1 = $GET ["numero"]
+    echo"<script>alert('O numero informado foi')</script>";
+
+
+
+
+
+
+    ?>
+</body>
+</html>
